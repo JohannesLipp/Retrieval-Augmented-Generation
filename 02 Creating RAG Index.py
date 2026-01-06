d17f4074c55300b0231039d5030357def057aa5a
Johannes Theissen-Lipp <johannes@johanneslipp.org>
2026-01-06 10:00:52 +0100

    Add function agents and RAG basics

---
+++ 02 Creating RAG Index.py
@@ -0,0 +1,22 @@
+from llama_index.core import StorageContext
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.llms.ollama import Ollama
+
+# Settings control global defaults
+Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
+Settings.llm = Ollama(
+    model="llama3.1",
+    request_timeout=360.0,
+    # Manually set the context window to limit memory usage
+    context_window=8000,
+)
+
+documents = SimpleDirectoryReader("data").load_data()
+index = VectorStoreIndex.from_documents(
+    documents,
+    # we can optionally override the embed_model here
+    # embed_model=Settings.embed_model,
+)
+
+index.storage_context.persist("storage")
